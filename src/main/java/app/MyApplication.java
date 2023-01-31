962cd7509c7eb815e0c28a615c3e480536820671
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-01-31 16:49:25 +0000

    Initial commit

---
+++ src/main/java/app/MyApplication.java
@@ -0,0 +1,16 @@
+package app;
+
+/**
+ * @author : ayoso
+ * @mailto : ayomide.sola-ayodele@ucdconnect.ie
+ * @since : 30/01/2023, Monday
+ **/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MyApplication.class, args);
+  }
+}
