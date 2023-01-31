0132449b80ac8bff179e4375d9f692e65ae56e69
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-01-31 16:56:21 +0000

    Test Commit

--- src/main/java/app/MyApplication.java
+++ src/main/java/app/MyApplication.java
@@ -8,6 +8,12 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 1. Run App in Intellj or Through command line
+// For command line, go to project source (aimarket folder) and then use the following command
+//  mvn spring-boot:run
+
+// 2. Go to this link: http://localhost:8080/.  A webpage should appear there now.
+
 @SpringBootApplication
 public class MyApplication {
   public static void main(String[] args) {
