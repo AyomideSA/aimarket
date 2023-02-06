6eff04e3f900fbf1d7b9c14b21159762ac17a010
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-06 16:50:28 +0000

    When "http://localhost:8080/aimarket/home" is visited, the website home page loads

---
+++ src/main/java/app/aimarket/MarketController.java
@@ -0,0 +1,16 @@
+package app.aimarket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/aimarket")
+public class MarketController {
+
+  @GetMapping("/home")
+  public String viewHomePage() {
+    return "/home.html";
+  }
+
+}
