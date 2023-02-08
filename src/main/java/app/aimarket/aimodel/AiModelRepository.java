ee5ead4e308141d0831e7e72922582523e568412
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-08 20:05:05 +0000

    Added SQL query to get only available models

---
+++ src/main/java/app/aimarket/aimodel/AiModelRepository.java
@@ -0,0 +1,19 @@
+package app.aimarket.aimodel;
+
+import app.aimarket.aimodel.AiModel;
+import app.aimarket.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/**
+ * @author : ayoso
+ * @mailto : ayomide.sola-ayodele@ucdconnect.ie
+ * @since : 08/02/2023, Wednesday
+ **/
+public interface AiModelRepository
+    extends JpaRepository<AiModel, Long> {
+  @Query("select m from AiModel m where m.availability = TRUE")
+  List<AiModel> getAvailableModels();
+}
