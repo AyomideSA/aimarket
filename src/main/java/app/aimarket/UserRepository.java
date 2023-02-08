049275cd6857ce57e452202c330bb79dae0182e1
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-08 12:44:48 +0000

    Repository for users created

--- src/main/java/app/aimarket/UserRepository.java
+++ src/main/java/app/aimarket/UserRepository.java
@@ -3,12 +3,10 @@ package app.aimarket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * @author : ayoso
- * @mailto : ayomide.sola-ayodele@ucdconnect.ie
- * @since : 07/02/2023, Tuesday
- **/
 @Repository
 public interface UserRepository extends
     JpaRepository<User, Long> {
+
+  User findByEmail(String user);
+
 }
