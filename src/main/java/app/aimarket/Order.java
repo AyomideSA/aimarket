dda5fda10010c32bb75287f7146298f2f1e6e3f6
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-08 12:42:54 +0000

    Added new constructor so orders can be created internally for testing

--- src/main/java/app/aimarket/Order.java
+++ src/main/java/app/aimarket/Order.java
@@ -3,6 +3,7 @@ package app.aimarket;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
@@ -12,13 +13,23 @@ import java.time.LocalDate;
  **/
 @Entity
 @Table(name="orders")
-public class Order {
+public class Order implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
-  @DateTimeFormat(pattern = "dd-MM-yyyy")
-  private LocalDate date;
-  private String status;
+  @Column private int userid;
+  @Column private LocalDate date;
+  @Column private String status;
+
+  public Order() {
+  }
+
+  public Order(int id, int userid, LocalDate date, String status) {
+    this.id = id;
+    this.userid = userid;
+    this.date = date;
+    this.status = status;
+  }
 
   public int getId() {
     return id;
@@ -44,10 +55,19 @@ public class Order {
     this.status = status;
   }
 
+  public int getUserId() {
+    return userid;
+  }
+
+  public void setUserId(int userId) {
+    this.userid = userId;
+  }
+
   @Override
   public String toString() {
     return "Order{" +
         "id=" + id +
+        ", userid=" + userid +
         ", date=" + date +
         ", status='" + status + '\'' +
         '}';
