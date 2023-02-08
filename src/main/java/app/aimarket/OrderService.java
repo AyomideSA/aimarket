377fef1f889cc27e5526eafc34721fa5babbc2a1
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-08 12:44:16 +0000

    Service Layer for orders created

---
+++ src/main/java/app/aimarket/OrderService.java
@@ -0,0 +1,35 @@
+package app.aimarket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+  private final OrderRepository orderRepository;
+
+  @Autowired
+  public OrderService(OrderRepository orderRepository) {
+    this.orderRepository = orderRepository;
+  }
+
+  public void save(@ModelAttribute Order order) {
+    orderRepository.save(order);
+  }
+
+  public void saveAll(List<Order> orders) {
+    orderRepository.saveAll(orders);
+  }
+
+  public List<Order> getOrders() {
+    return orderRepository.findAll();
+  }
+
+  public List<Order> findByUserId(int id) {
+    return orderRepository.findByUserId(id);
+  }
+
+}
