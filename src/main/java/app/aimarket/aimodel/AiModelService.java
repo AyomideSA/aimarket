fa2d858e0a18098db74ddbb3c4f5291ff0d08b92
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-08 20:05:28 +0000

    Service layer for AiModelRepository

---
+++ src/main/java/app/aimarket/aimodel/AiModelService.java
@@ -0,0 +1,28 @@
+package app.aimarket.aimodel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author : ayoso
+ * @mailto : ayomide.sola-ayodele@ucdconnect.ie
+ * @since : 08/02/2023, Wednesday
+ **/
+
+@Service
+public class AiModelService {
+
+  private final AiModelRepository aiModelRepository;
+
+  @Autowired
+  public AiModelService(AiModelRepository aiModelRepository) {
+    this.aiModelRepository = aiModelRepository;
+  }
+
+  public List<AiModel> getAvailabileModels() {
+    return aiModelRepository.getAvailableModels();
+  }
+
+}
