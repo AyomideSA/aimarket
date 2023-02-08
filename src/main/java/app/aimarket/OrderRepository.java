b747b5185aa17790c11c6e8640a8477e6e7ef745
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-08 12:43:40 +0000

    Repository for orders created

---
+++ src/main/java/app/aimarket/OrderRepository.java
@@ -0,0 +1,18 @@
+package app.aimarket;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/**
+ * @author : ayoso
+ * @mailto : ayomide.sola-ayodele@ucdconnect.ie
+ * @since : 07/02/2023, Tuesday
+ **/
+public interface OrderRepository extends
+    JpaRepository<Order, Long> {
+
+  @Query("select o from Order o where o.userid = ?#{[0]}")
+  List<Order> findByUserId(int id);
+}
