25db1288de6595bc44158caa9d4ff27620bad4e0
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-02-09 10:47:01 +0000

    Added new models for testing

--- src/main/java/app/aimarket/aimodel/AiModelConfig.java
+++ src/main/java/app/aimarket/aimodel/AiModelConfig.java
@@ -22,7 +22,7 @@ public class AiModelConfig {
           50.00,
           70.00,
           "A pretty good ai",
-          true,
+          false,
           "/pictures/auto.jpg"
       );
       AiModel cortana = new AiModel(
@@ -31,10 +31,46 @@ public class AiModelConfig {
           51.00,
           71.00,
           "A really good ai",
-          false,
+          true,
           "/pictures/cortana.jpeg"
       );
-      aiModelRepository.saveAll(List.of(auto, cortana));
+      AiModel irobot = new AiModel(
+          3L,
+          "irobot",
+          22.00,
+          33.00,
+          "A really good ai model",
+          true,
+          "/pictures/irobot.png"
+      );
+      AiModel jarvis = new AiModel(
+          4L,
+          "jarvis",
+          30.00,
+          40.00,
+          "The Best AI model",
+          true,
+          "/pictures/jarvis.png"
+      );
+      AiModel stockai = new AiModel(
+          5L,
+          "jarvis",
+          30.00,
+          40.00,
+          "The Best AI stock",
+          true,
+          "/pictures/stockai.jpg"
+      );
+      AiModel ultron = new AiModel(
+          6L,
+          "ultron",
+          40.00,
+          50.00,
+          "ULTRON",
+          true,
+          "/pictures/ultron.jpg"
+      );
+      aiModelRepository.saveAll(List.of(auto, cortana, irobot, jarvis, stockai, ultron));
     };
   }
 }
