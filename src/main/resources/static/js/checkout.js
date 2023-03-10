a8da79d5cb066d3cebc6cbc3692ceb0cc3918077
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2023-03-10 14:10:13 +0000

    Checkout Box added to basket page

---
+++ src/main/resources/static/js/checkout.js
@@ -0,0 +1,9 @@
+function closeCheckout() {
+    document.getElementById("checkoutBox").style.display = "none";
+    document.getElementById("enterCheckoutDetails").style.display = "none";
+}
+
+function openCheckout() {
+    document.getElementById("checkoutBox").style.display = "block";
+    document.getElementById("enterCheckoutDetails").style.display = "block";
+}
